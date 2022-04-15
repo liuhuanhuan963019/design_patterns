0ab713d728e0723f6c21f00806ba117704741ea3
”liuhuanhuan963019” <2432783449@qq.com>
2022-04-15 16:25:59 +0800

    破坏单例模式解决方式

--- src/main/java/com/lhh/pattern/singleton/demo07/Singleton.java
+++ src/main/java/com/lhh/pattern/singleton/demo07/Singleton.java
@@ -21,4 +21,10 @@ public class Singleton implements Serializable {
     public static Singleton getInstance() {
         return SingletonHolder.INSTANCE;
     }
+
+    // 解决单例被破坏的问题
+    // 当进行反序列化时，会自动调用该方法，将该方法的返回值直接返回
+    public Object readResolve() {
+        return SingletonHolder.INSTANCE;
+    }
 }
