e77dde30910edfa0da9713f1111ad42d6d85f228
”liuhuanhuan963019” <2432783449@qq.com>
2022-04-15 20:58:13 +0800

    原型对象深克隆

---
+++ src/main/java/com/lhh/pattern/realizetype/Client.java
@@ -0,0 +1,15 @@
+package com.lhh.pattern.realizetype;
+
+/**
+ * @author liuhuanhuan
+ * @version 1.0
+ * @date 2022/4/15 20:56
+ * @Description
+ */
+public class Client {
+    public static void main(String[] args) throws CloneNotSupportedException {
+        RealizeType realizeType = new RealizeType();
+        RealizeType clone = realizeType.clone();
+        System.out.println("判断两个对象地址是否相同" + (realizeType==clone));
+    }
+}
---
+++ src/main/java/com/lhh/pattern/realizetype/RealizeType.java
@@ -0,0 +1,20 @@
+package com.lhh.pattern.realizetype;
+
+/**
+ * @author liuhuanhuan
+ * @version 1.0
+ * @date 2022/4/15 20:55
+ * @Description
+ */
+public class RealizeType implements Cloneable{
+
+    public RealizeType() {
+        System.out.println("具体的原型对象创建成功");
+    }
+
+    @Override
+    public RealizeType clone() throws CloneNotSupportedException {
+        System.out.println("复制的对象创建成功");
+        return (RealizeType) super.clone();
+    }
+}
